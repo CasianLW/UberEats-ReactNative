befd7f65e7f5548c452971af185ff815295fe232
CasianLW <ciorbcasian@gmail.com>
2023-11-12 19:13:12 +0100

    svg fix (still not working)

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,37 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+// const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
+// /**
+//  * Metro configuration
+//  * https://facebook.github.io/metro/docs/configuration
+//  *
+//  * @type {import('metro-config').MetroConfig}
+//  */
+// const config = {};
+
+// module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+
+const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
 
 /**
- * Metro configuration
+ * Metro configuration for React Native
  * https://facebook.github.io/metro/docs/configuration
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+  const config = {
+    transformer: {
+      babelTransformerPath: require.resolve("react-native-svg-transformer"),
+    },
+    resolver: {
+      assetExts: assetExts.filter((ext) => ext !== "svg"),
+      sourceExts: [...sourceExts, "svg"],
+    },
+  };
+
+  return mergeConfig(await getDefaultConfig(__dirname), config);
+})();
