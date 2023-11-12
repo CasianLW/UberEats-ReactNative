2257aa3617cfbd6aebb4ce10752d46ffe1dea07c
CasianLW <ciorbcasian@gmail.com>
2023-11-12 19:15:14 +0100

    added state and new components

--- pages/HomepageComponent.tsx
+++ pages/HomepageComponent.tsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { FC, useState } from "react";
 import { View, Text, ScrollView, StyleSheet } from "react-native";
 import CategoriesSectionComponent from "../components/CategoriesSectionComponent";
 import OffersSectionComponent from "../components/OffersSectionComponent";
 import ListeCategoriesProduitsComponent from "../components/ListeCategorieProduitsComponent";
+import NavComponent from "../components/NavComponent";
+import DishesListComponent from "../components/DishesListComponent";
 
-const HomepageComponent: React.FC = () => {
+const HomepageComponent: FC = () => {
+  const [cartItemCount, setCartItemCount] = useState(0);
+  const addItemToCart = () => {
+    setCartItemCount(cartItemCount + 1);
+  };
+  const removeItemFromCart = () => {
+    setCartItemCount(cartItemCount - 1);
+  };
   return (
     <ScrollView style={styles.container}>
       <View style={styles.main}>
+        <NavComponent cartItemCount={cartItemCount} />
+
         <CategoriesSectionComponent />
+        <DishesListComponent
+          addToCart={addItemToCart}
+          removeFromCart={removeItemFromCart}
+          cartItemCount={cartItemCount}
+        />
         <OffersSectionComponent />
 
         <Text>Contenu de la page</Text>
